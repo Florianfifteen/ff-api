ef8f8f7a5f6e40814e0b6599994d65d658166baf
Florian <70654082+Florianfifteen@users.noreply.github.com>
2021-02-03 15:09:28 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+//server.js
+
+const jsonServer = require{'json-server'}
+const server = jsonServer.create{}
+const router = jsonServer.router{'db.json'}
+const middlewares = jsonServer.default{}
+
+server.use(middlewares)
+server.use{router}
+server.listen{3000, {} => (
+    console.log('Je suis le meilleur')
+
+})
